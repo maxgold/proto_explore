0a80d0e4c4f329ffa7f65bbf5df2e194c6e86c59
Nina <nina.mortensen@stern.nyu.edu>
2023-03-03 14:58:42 -0500

    .

--- url_benchmark/agent/ddpg.py
+++ url_benchmark/agent/ddpg.py
@@ -427,7 +427,8 @@ class DDPGAgent:
         #TODO: change this part to be compatible with the new replay buffer
         obs, obs_state, action, reward, discount, next_obs, next_obs_state, goal, goal_state = utils.to_torch(
             batch, self.device)
-        
+        print('obs', obs.shape) 
+        print('step', step)
         # augment and encode
         if obs.shape[0]!=1:
             obs = obs[None,:]
