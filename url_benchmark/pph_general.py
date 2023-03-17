3edf1c64d4caa74910557dace3a69b87d88afd7e
Nina Mortensen <nina.mortensen@stern.nyu.edu>
2023-03-17 14:37:19 -0400

    .

--- url_benchmark/pph_general.py
+++ url_benchmark/pph_general.py
@@ -586,10 +586,8 @@ class Workspace:
         if self.pmm == False:
             time_step_no_goal = None
 
-        if self.cfg.model_path and self.cfg.offline_gc is False:
+        if self.cfg.model_path:
             self.evaluate(eval=True)
-        elif self.cfg.model_path:
-            self.evaluate()
 
         while train_until_step(self.global_step):
 
