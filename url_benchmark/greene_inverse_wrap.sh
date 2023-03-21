df644c90840f6a43b1335ac92e577dfe8b604f3b
Nina Mortensen <nm1874@log-2.hpc.nyu.edu>
2023-03-21 11:42:11 -0400

    .

--- url_benchmark/greene_inverse_wrap.sh
+++ url_benchmark/greene_inverse_wrap.sh
@@ -35,6 +35,6 @@ python pph_general.py agent=ddpg domain=point_mass_maze batch_size=256 num_proto
 num_seed_frames=2000 replay_buffer_size=1000000 hidden_dim=256 seed=0 gc_only=True load_encoder=True inv=True feature_dim_gc=50 encoder1=True sl=False \
 use_critic_trunk=True offline_gc=True expert_buffer=False offline_model_step=$offline_model_step greene=True init_from_proto=True pretrained_feature_dim=16 \
 eval_every_frames=100000 model_path=/exp_local/2023.03.06/144540_proto/optimizer_proto_1000000.pth goal_offset=$offset tmux_session=greene_batch frame_stack=1 \
-feature_dim=50 egocentric=False camera_id=0 debug=True"
+feature_dim=50 egocentric=False camera_id=0 eval_proto_goals=True"
 EOT
 #echo $msg
